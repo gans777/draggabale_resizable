80ddcf6c225c0b62d33d2831520064cbfe3af508
gans777 <animekogan@gmail.com>
2022-01-05 00:41:47 +0300

    born window near parent

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<template>
+<template >
   <div id="app">
    <b-container>
-  <b-row ><b-col ><div class="wrap_place plas_place"> <b-icon icon="plus-square" variant="success" class="h1 mb-0" @click="add_directory"></b-icon></div>
+  <b-row ><b-col ><div class="wrap_place plas_place"> <b-icon icon="plus-square" variant="success" class="h1 mb-0" @click="add_directory()"></b-icon></div>
 <b-modal id="create_dir" title="BootstrapVue">
     <p class="my-4">
       <input v-model.trim="new_window_text" type="text" value="писать текст здесь">
@@ -27,7 +27,7 @@
     <div style="height: 500px;width:100%; position: relative;">
     <vue-draggable-resizable v-for="(coord,index) in everythinkDraggCoords" :key="index"   :x="coord.x" :y="coord.y" :w="coord.w" :h="coord.h"   :parent="true" @activated="onActivated(index)" @dragging="onDrag" @resizing="onResize"  @deactivated = "onDeactivated()"  style="border-radius:10px;border:2px solid black;">
       <div class="into_block">{{ coord.text }}<hr>
-<div class="wrap_place plas_place"> <b-icon icon="plus-square" variant="success" class="h1 mb-0" @click="add_directory"></b-icon></div>
+<div class="wrap_place plas_place"> <b-icon icon="plus-square" variant="success" class="h1 mb-0" @click="add_directory(coord)"></b-icon></div>
         <hr> X: {{ coord.x }} / Y: {{ coord.y }} - Width: {{ coord.w }} / Height: {{ coord.h }}
       </div >
     </vue-draggable-resizable>
@@ -53,11 +53,9 @@ export default {
         new_window_text:'',
         last_id:null,
         id_block_emphasized: null,
+        line_everythinkDraggCoords:{},
         everythinkDraggCoords:[
-                {id:0,x:250,y:250,w:100,h:100,text:'окно 0'},
-                {id:1,x:55,y:0,w:100,h:150,text:'окно c id=1'},
-                {id:2,x:55,y:200,w:100,h:200,text:'lorem ipsum'}
-                            ]
+                               ]
         }
       },
       mounted() {
@@ -100,16 +98,32 @@ export default {
           this.tmp_localStorage_Clear=true;
 
         },
-        add_directory(){
+        add_directory(coord){
+          if (coord ===undefined){
+            this.line_everythinkDraggCoords={};
+          } else {this.line_everythinkDraggCoords=coord;}
           this.$bvModal.show('create_dir');
-
-        },
+          },
         create_new_window(){
           console.log('создаю окно'+ this.everythinkDraggCoords.length);
-         this.everythinkDraggCoords.push({id:this.everythinkDraggCoords.length, x:0,y:0,w:100,h:100,text:this.new_window_text});
+          if (this.isEmpty(this.line_everythinkDraggCoords)){
+            this.everythinkDraggCoords.push({id:this.everythinkDraggCoords.length, x:0,y:0,w:100,h:100,text:this.new_window_text});
+          }
+         else{
+          this.everythinkDraggCoords.push({id:this.everythinkDraggCoords.length, x:this.line_everythinkDraggCoords.x+5,y:this.line_everythinkDraggCoords.y+5,w:100,h:100,text:this.new_window_text});
+         }
+         
          this.$bvModal.hide('create_dir');
          this.new_window_text='';
-        }
+        },
+         isEmpty(obj) {
+         for (let key in obj) {
+          // если тело цикла начнет выполняться - значит в объекте есть свойства
+          return false;
+            }
+          return true;
+          }
+
       }
 }
 </script>
